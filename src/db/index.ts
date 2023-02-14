fe3f36f941a0a61dba08b25baf754f290d7c95d6
Benjamin Dickinson <bsadickinson@gmail.com>
2023-02-13 22:33:08 -0600

    commit to push after partial db connection setup

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,3 +1,27 @@
-import path from 'path';
-import passport from 'passport';
-import GoogleStrategy from 'passport-google-oauth20'
+//import path from 'path';
+//import passport from 'passport';
+//import GoogleStrategy from 'passport-google-oauth20';
+//import { Sequelize } from 'sequelize-typescript'; //UNINSTALL ME?
+import 'dotenv/config';
+//import schemas
+import { Sequelize } from 'sequelize'
+
+const dbUser = process.env.DB_USER as string;
+const dbHost = process.env.DATABASE_URL as string;  //uncomment to use second sequelize call for using external db
+
+//uncomment second sequelize call for using external db
+const sequelize = new Sequelize('gnawlinz', dbUser, '', {
+  host: dbHost,
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
+});
+
+sequelize.authenticate()
+  .then(() => console.log('Connection has been established successfully.'))
+  .catch((error) => console.error('Unable to connect to the database:', error));
