a750e1f0d0a0f85a916613ffeda36a98927e5c42
Benjamin Dickinson <bsadickinson@gmail.com>
2023-02-15 12:30:07 -0600

    add all needed columns to User model

--- src/db/user.ts
+++ src/db/user.ts
@@ -2,17 +2,41 @@ import axios from 'axios';
 import {DataTypes } from 'sequelize';
 import { db } from './index';
 const User = db.define('user', {
-  googleId: {
-    type: DataTypes.STRING,
-  },
   user_id: {
     type: DataTypes.INTEGER,
     autoIncrement: true,
     allowNull: false,
     primaryKey: true
   },
+  googleId: {
+    type: DataTypes.STRING
+  },
   name: {
     type: DataTypes.STRING
+  },
+  avatar: {
+    type: DataTypes.STRING
+  },
+  char0: {
+    type: DataTypes.STRING, //INTEGER?
+    references: {
+      //model: Character, //uncomment this when the character model is in the DB
+      key: 'id'
+    }
+  },
+  char1: {
+    type: DataTypes.STRING, //INTEGER?
+    references: {
+      //model: Character, //uncomment this when the character model is in the DB
+      key: 'id'
+    }
+  },
+  char2: {
+    type: DataTypes.STRING, //INTEGER?
+    references: {
+      //model: Character, //uncomment this when the character model is in the DB
+      key: 'id'
+    }
   }
 });
 
