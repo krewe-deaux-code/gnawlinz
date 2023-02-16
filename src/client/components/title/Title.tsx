aa8bd7e4685261de9c802febb3ee963dc9c52851
Matthew <matthewjschiro@gmail.com>
2023-02-15 22:26:36 -0600

    merge with upstream

--- src/client/components/title/Title.tsx
+++ src/client/components/title/Title.tsx
@@ -1,10 +1,25 @@
 import React from "react";
-import { TitleContainer, TopBar, Main, ContentBox, Content1, Content2, Content3, Footer } from './Styled';
+// import dayjs from 'dayjs';
+import { TitleContainer, TopBar, Main, Footer } from './Styled';
 
 // Need logo - top left, title image - center, possibly login pic - top right
 // Clock above title image, start button bottom center
 
 const Title: React.FC = () => {
+
+// dayjs 72 hour function
+// const startTime = dayjs();
+// const endTime = startTime.add(72, 'hour');
+
+// function updateTimer() {
+//   const remainingTime = endTime.diff(dayjs(), 'millisecond');
+//   const remainingHours = Math.floor(remainingTime / (1000 * 60 * 60));
+//   const remainingMinutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
+//   const remainingSeconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
+//   console.log(`${remainingHours} hours, ${remainingMinutes} minutes, ${remainingSeconds} seconds remaining`);
+// }
+
+// setInterval(updateTimer, 1000);
 
   return (
     <TitleContainer>
