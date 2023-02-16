1030e7720728d99a028b8065dbfac035a37ad0e7
Matthew <matthewjschiro@gmail.com>
2023-02-15 18:46:49 -0600

    change format into 3 containers

--- src/client/title/Title.tsx
+++ src/client/title/Title.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { TitleContainer, Item } from './Styled';
+import { TitleContainer, TopBar, Main, ContentBox, Content1, Content2, Content3, Footer } from './Styled';
 
 // Need logo - top left, title image - center, possibly login pic - top right
 // Clock above title image, start button bottom center
@@ -8,14 +8,22 @@ const Title: React.FC = () => {
 
   return (
     <TitleContainer>
-      <Item>
-    <div>
-      <h1>Title Screen</h1>
-      <button>
+      <TopBar>TopBar</TopBar>
+ <Main>Title Image</Main>
+
+ {/* <ContentBox>
+     <Content1>Content1</Content1>
+     <Content2>Content2</Content2>
+     <Content3>Content3</Content3>
+ </ContentBox> */}
+ <Footer>
+    <button>
       <a href="/auth/google">Start</a>
-      </button>
-    </div>
-      </Item>
+    </button>
+ </Footer>
+
+
+
     </TitleContainer>
   )
 };
