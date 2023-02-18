f2e0c4ade0b9015003fecd77bb81d0afc907cf84
Miles Ager <miles.ager13@gmail.com>
2023-02-17 20:44:32 -0600

    making object

---
+++ src/db/seeders/seedData/locationSeed.ts
@@ -0,0 +1,7 @@
+export const locationSeed = [
+  {
+    name: "Chompitoulas Street",
+    image: "https://i0.wp.com/beneworleans.com/wp-content/uploads/2018/10/tchoup.jpg?fit=640%2C480&ssl=1",
+   
+  }
+];
