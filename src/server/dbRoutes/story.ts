14da83a947204adf14cdc39965f98bf5d21deda7
Benjamin Dickinson <bsadickinson@gmail.com>
2023-02-18 20:25:07 -0600

    create skeleton of end views and return story data

--- src/server/dbRoutes/story.ts
+++ src/server/dbRoutes/story.ts
@@ -4,6 +4,7 @@ import { Router } from 'express';
 const storyRouter = Router();
 
 // <-- Unsure if we need these -->
+
 //import { Sequelize } from 'sequelize';
 // import { db } from '../../db/index';
 // import '../auth/auth';
@@ -19,11 +20,14 @@ storyRouter.use(express.urlencoded({ extended: true }));
 // *** DB Queries ***
 // ******************
 
-storyRouter.get('/ending', () => {
-  Story.findOne({
-    where: {
-      character_id: 1
-    }
+storyRouter.get('/ending/:charID', (req, res) => {
+  Story.findOne({ where: { character_id: req.params.charID }})
+  .then((response) => {
+    console.log('story object retrieved from db: ', response);
+    res.status(200).send(response);
+  })
+  .catch((err) => {
+    console.error(err, 'server failed to retrieve story from DB');
   })
 })
 
