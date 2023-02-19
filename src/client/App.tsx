14c1f1d788874d5165671166635ab27ee62cb621
Matthew <matthewjschiro@gmail.com>
2023-02-18 21:42:01 -0600

    add context for clock, add to game view and title

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,25 +1,51 @@
-import React, { Suspense, lazy } from "react"; //lazy
+import React, { Suspense, lazy, createContext, useState } from "react"; //lazy
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'; //Routes, Route, Navigate
+import dayjs from 'dayjs';
 
 const Title = lazy(() => import('./components/title/Title'));
 const Menu = lazy(() => import('./components/menu/Menu'));
 const GameView = lazy(() => import('./components/gameView/GameView'));
 
+export const ClockContext = createContext<any>('')
 
-const App = () => (
-  <BrowserRouter>
-    <Suspense fallback={<div>LOADING...</div>}>
+const App = () => {
 
-      <Routes>
-        <Route path='/' element={<Title />} />
-        <Route path='menu' element={<Menu />} />
-        <Route path='gameView' element={<GameView />} />
-        <Route path='*' element={<Navigate to='/' replace />} />
-      </Routes>
-      {/* <Menu /> */}
-      {/* <a href="/auth/google">authenticate that typescript is great</a> */}
-    </Suspense>
-  </BrowserRouter>
-);
+  const [remainingTime, setRemainingTime] = useState<any>('');
 
+  function calculateRemainingTime() {
+    let interval = setInterval(() => {
+      let daysLeft = 3;
+      let startTime = dayjs();
+      let endTime = startTime.add( daysLeft, 'day').startOf('day');
+      let remainingTime = endTime.diff(dayjs(), 'millisecond');
+      let remainingHours = Math.floor(remainingTime / (1000 * 60 * 60));
+      let remainingMinutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
+      let remainingSeconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
+
+      let formattedTime = `${remainingHours} hours, ${remainingMinutes} minutes, ${remainingSeconds} seconds`;
+      setRemainingTime(formattedTime);
+
+    }, 1000);
+    return () => clearInterval(interval);
+  };
+
+  return (
+    <ClockContext.Provider value={{remainingTime, setRemainingTime, calculateRemainingTime}} >
+      <BrowserRouter>
+        <Suspense fallback={<div>LOADING...</div>}>
+
+          <Routes>
+            <Route path='/' element={<Title />} />
+            <Route path='menu' element={<Menu />} />
+            <Route path='gameView' element={<GameView />} />
+            <Route path='*' element={<Navigate to='/' replace />} />
+          </Routes>
+
+          {/* <Menu /> */}
+          {/* <a href="/auth/google">authenticate that typescript is great</a> */}
+        </Suspense>
+      </BrowserRouter>
+    </ClockContext.Provider>
+)
+};
 export default App;
--- src/client/components/title/Title.tsx
+++ src/client/components/title/Title.tsx
@@ -1,35 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useContext } from "react";
 //import { Link } from 'react-router-dom';
-import dayjs from 'dayjs';
+
 import { TitleContainer, TopBar, Main, Content1, Content2, Content3, Footer } from './Styled';
 //import GameView from '../gameView/GameView';
-
+import { ClockContext } from "../../App";
 // Need logo - top left, title image - center, possibly login pic - top right
 // Clock above title image, start button bottom center
 
 const Title: React.FC = () => {
 
   // dayjs 72 hour function
-  let daysLeft = 3;
-  const startTime = dayjs();
-  const endTime = startTime.add( daysLeft, 'day').startOf('day');
-  const [remainingTime, setRemainingTime] = useState<string>('');
 
-  function calculateRemainingTime() {
-    const remainingTime = endTime.diff(dayjs(), 'millisecond');
-    const remainingHours = Math.floor(remainingTime / (1000 * 60 * 60));
-    const remainingMinutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
-    const remainingSeconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
+  const {remainingTime, calculateRemainingTime} = useContext(ClockContext);
 
-    const formattedTime = `${remainingHours} hours, ${remainingMinutes} minutes, ${remainingSeconds} seconds`;
-    setRemainingTime(formattedTime);
-  }
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      calculateRemainingTime();
-    }, 1000);
-    return () => clearInterval(interval);
+    calculateRemainingTime();
   }, []);
 
   return (
