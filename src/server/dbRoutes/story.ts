e61ce303fc5e04f8b4c95a5324e8d10aa04381c4
Benjamin Dickinson <bsadickinson@gmail.com>
2023-02-18 22:58:08 -0600

    commit to pull

--- src/server/dbRoutes/story.ts
+++ src/server/dbRoutes/story.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { Router } from 'express';
-import { TEXT } from 'sequelize';
+//import { TEXT } from 'sequelize';
 import Choice from '../../db/schemas/choice';
 
 const storyRouter = Router();
@@ -27,16 +27,24 @@ storyRouter.get('/ending/:charID', (req, res) => {
     .then((storyResponse: any) => {
       console.log('story object retrieved from db: ', storyResponse);
       //build ending text to return in .send()
-      let storyArr = [];
-      for (let i = 0; i < storyResponse.char_choices.length; i++) {
-        Choice.findOne({ where: { _id: storyResponse.char_choices[i] } })
-          .then((choiceResponse) => {
-            if (choiceResponse) {
-              storyArr.push(choiceResponse.flavor_text0)
-            }
+      let storyArr: Number[] = storyResponse.char_choices;
+      console.log('choices array: ', storyArr);
+      let choiceArr: String[] = storyArr.map((choiceId: Number) => {
+        return Choice.findOne({where:{_id: choiceId}})
+          .then((choiceResponse: any) => {
+            
           })
-          .catch((err) => console.error('choiceGrab.failHard: ', err))
-      }
+      })
+      console.log('Choices Text Array: ', choiceArr);
+      // for (let i = 0; i < storyResponse.char_choices.length; i++) {
+      //   Choice.findOne({ where: { _id: storyResponse.char_choices[i] } })
+      //     .then((choiceResponse: any) => {
+      //       if (choiceResponse) {
+      //         storyArr.push(choiceResponse.flavor_text0)
+      //       }
+      //     })
+      //     .catch((err) => console.error('choiceGrab.failHard: ', err))
+      // }
       res.status(200).send(storyArr); //return array
     })
     .catch((err) => {
