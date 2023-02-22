357cba052d0d93b40fe3f1907069e69bffc9fe91
Benjamin Dickinson <bsadickinson@gmail.com>
2023-02-22 13:11:27 -0600

    clock no longer rerenders whole page

--- src/client/components/nav/NavBar.tsx
+++ src/client/components/nav/NavBar.tsx
@@ -1,13 +1,35 @@
 //import axios from 'axios';
-import React, { useEffect, useContext } from 'react'; //useState, 
+import React, { useEffect, useState } from 'react'; //useState, 
 import { NavBar, TopContent1, TopContent2, TopContent3 } from './Styled'; //ContentBox
 
 import { Link } from 'react-router-dom';
-import { ClockContext } from "../../App";
+import dayjs from 'dayjs';
 
 const Nav: React.FC = () => {
   //do stuff
-  const {remainingTime, calculateRemainingTime} = useContext(ClockContext);
+
+  const [remainingTime, setRemainingTime] = useState<any>('');
+
+  function calculateRemainingTime() {
+    let interval = setInterval(() => {
+      let daysLeft = 3;
+      let startTime = dayjs();
+      let endTime = startTime.add( daysLeft, 'day').startOf('day');
+      let remainingTime = endTime.diff(dayjs(), 'millisecond');
+      let remainingHours = Math.floor(remainingTime / (1000 * 60 * 60));
+      let remainingMinutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
+      let remainingSeconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
+
+      let formattedTime = `${remainingHours} hours, ${remainingMinutes} minutes, ${remainingSeconds} seconds`;
+      setRemainingTime(formattedTime);
+
+    }, 1000);
+    return () => clearInterval(interval);
+  };
+
+
+
+  //const {remainingTime, calculateRemainingTime} = useContext(ClockContext);
   useEffect(() => {
     calculateRemainingTime();
   }, []);
