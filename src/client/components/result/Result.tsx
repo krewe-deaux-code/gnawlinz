0ceb6e6a5e3e04924b3e140c632073abee489596
Sean Niehus <seanniehus@gmail.com>
2023-02-22 15:44:37 -0600

    fetch story from database, render to result component

--- src/client/components/result/Result.tsx
+++ src/client/components/result/Result.tsx
@@ -1,22 +1,37 @@
-//CURRENTLY A COPY OF MENU
-
-import React from "react";//createContext, useContext
-// import { Link} from 'react-router-dom';
+import React, { useContext, useEffect, useState } from "react";//createContext, useContext
+// import { Link } from 'react-router-dom';
 import { Container, NavBar, Story, End } from './Styled';
-// import axios from 'axios';
+
+import { UserContext } from "../../App"; // <-- holds User object
+
+
+import axios from 'axios';
 
 const Result: React.FC = () => {
 
+  const { currentChar, } = useContext(UserContext); // <-- NEED to get user chars below
 
-  //axios request to server to retrieve avatar
-  //set google id to a string
+  const [ story, setStory ] = useState([]);
 
 
+
+  useEffect(() => {
+    axios.get(`story/ending/${currentChar._id}`)
+      .then((results) => {
+        setStory(results.data)
+        console.log('here>>>>>>>>>>>', results.data)
+      }).catch((err) => {
+        console.error(err);
+      });
+
+  }, []);
+
+  console.log('and here>>>>>>>>>>>>', story);
 
   return (
     <Container>
       <NavBar>NavBar</NavBar>
-      <Story>Story</Story>
+      <Story>{ story }</Story>
       <End>Win/Lose</End>
     </Container>
   )
