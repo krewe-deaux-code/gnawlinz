e1c4f65f429c51fe96137f7b30fec693ba78e424
Matthew <matthewjschiro@gmail.com>
2023-02-24 12:27:07 -0600

    remove commented out code

--- src/client/components/title/Title.tsx
+++ src/client/components/title/Title.tsx
@@ -4,27 +4,12 @@ import Nav from "../nav/NavBar";
 import { TitleContainer, Main, Footer } from './Styled';
 //import GameView from '../gameView/GameView';
 //import axios from 'axios';
-//import { ClockContext } from "../../App";
-// Need logo - top left, title image - center, possibly login pic - top right
-// Clock above title image, start button bottom center
+
+
 
 const Title: React.FC = () => {
 
-  // dayjs 72 hour function
 
-  //const {remainingTime, calculateRemainingTime} = useContext(ClockContext);
-
-
-  // useEffect(() => {
-  //   calculateRemainingTime();
-  // }, []);
-
-
-  //<TopBar>
-  //    <Content1><h1>Logo</h1></Content1>
-  //  <Content2><h1>CLOCK GOES HERE</h1></Content2> {/** <h1>{remainingTime}*/}
-  //<Content3><h1>Google User</h1></Content3>
-  // </TopBar>
 
   return (
     <TitleContainer>
