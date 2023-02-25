2a2bf406ea8b32d0fe769c27a3b6f743fe90e21b
Matthew <matthewjschiro@gmail.com>
2023-02-25 10:27:35 -0600

    add pseudo for fetch all locations function

--- src/client/components/gameView/GameView.tsx
+++ src/client/components/gameView/GameView.tsx
@@ -14,9 +14,14 @@ import { UserContext } from '../../App';
 import { statCheck } from '../../utility/gameUtils';
 
 interface LocationData {
-  data: object;
-  image_url: string;
+  _id: number;
   name: string;
+  image_url: string;
+  random_item_spot1: string;
+  random_item_spot2: string;
+  drop_item_slot: number;
+  graffiti: string;
+  graffiti_msg: string;
 }
 
 interface EventData {
@@ -55,6 +60,8 @@ const GameView: React.FC = () => {
     evacuate: 0,
     wildcard: 0
   });
+  const [allLocations, setAllLocations] = useState<LocationData[]>([]);
+  const [visited, setVisited] = useState<LocationData[]>([]);
 
   const fetchEvent = () => {
     axios.get<EventData>('/event/random')
@@ -82,6 +89,12 @@ const GameView: React.FC = () => {
       })
       .catch(err => console.log('Axios fail useEffect Location grab', err));
   };
+  // fetch locations /locations/all
+  // axios fetch locations
+    // query db to find Locations.findall
+    // res.send (allLocations)
+  // axios fetch locations then
+  // set locations pass in all locations from res.send
 
   const resolveChoice = (index: number, stat: number, penalty = '') => {
     axios.get<ChoiceData>(`/choice/selected/${index}`)
@@ -117,7 +130,9 @@ const GameView: React.FC = () => {
   useEffect(() => {
     fetchLocation();
   }, []);
-  if (currentChar.health < 1) {
+
+  // conditional for character loss involving health or mood reaching 0
+  if (currentChar.health < 1 || currentChar.mood < 1) {
     return <div><Result/></div>;
   }
   console.log('CURRENT CHAR', currentChar);
@@ -161,7 +176,7 @@ const GameView: React.FC = () => {
           </Link>
           <Link to="/gameView" style={{ textDecoration: 'none' }}>
             <Content1>
-              <HudButton>New Location</HudButton> {/**previously Investigate*/}
+              <HudButton onClick={() => fetchLocation()}>New Location</HudButton> {/**previously Investigate*/}
             </Content1>
           </Link>
           <Content1>
