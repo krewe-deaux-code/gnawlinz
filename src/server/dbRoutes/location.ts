92147ae3a8f5668113996a2d04bc84f216b4a786
Miles Ager <miles.ager13@gmail.com>
2023-02-26 00:02:33 -0600

    create location put request

--- src/server/dbRoutes/location.ts
+++ src/server/dbRoutes/location.ts
@@ -1,10 +1,16 @@
 // import express from 'express';
-import { Router } from 'express';
+import express, { Router, Request, Response } from 'express';
 
 const locationRouter = Router();
 
-// <-- Unsure if we need these -->
+interface LocationUpdateRequest extends Request {
+  body: {
+    drop_item_slot: number;
+  };
+}
 
+
+// <-- Unsure if we need these -->
 import { Sequelize } from 'sequelize';
 // import { db } from '../../db/index';
 // import '../auth/auth';
@@ -15,12 +21,18 @@ import Location from '../../db/schemas/location';
 
 // <-- middleware -->
 
-// locationRouter.use(express.json());
-// locationRouter.use(express.urlencoded({ extended: true }));
+locationRouter.use(express.json());
+locationRouter.use(express.urlencoded({ extended: true }));
+
+
 
 // ******************
 // *** DB Queries ***
 // ******************
+
+
+
+
 
 locationRouter.get('/random', (req, res) => {
   Location.findOne({ order: Sequelize.literal('RANDOM()'), limit: 1 })
@@ -33,4 +45,25 @@ locationRouter.get('/random', (req, res) => {
     });
 });
 
+locationRouter.put('/:_id', async (req: LocationUpdateRequest, res: Response) => {
+  try {
+    const {_id} = req.params;
+    const {drop_item_slot} = req.body;
+
+    // Find the location by _id and update the drop_item_slot
+    const [rowsUpdated, [updatedLocation]] = await Location.update(
+      { drop_item_slot: drop_item_slot },
+      { where: { _id: _id }, returning: true },
+    );
+
+    if (rowsUpdated > 0) {
+      res.status(200).send(updatedLocation);
+    } else {
+      res.status(404).send(`Location ${_id} not found`);
+    }
+  } catch (err) {
+    console.error('Error in locationRouter.put--src/server/dbRoutes/location.ts', err);
+    res.status(500).send('Server error');
+  }
+});
 export default locationRouter;
