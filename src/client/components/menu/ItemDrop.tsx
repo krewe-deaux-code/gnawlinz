fd89dbd82cbceab0ebf47947a52b19eb00134f25
Benjamin Dickinson <bsadickinson@gmail.com>
2023-02-28 13:57:33 -0600

    commit to pull

--- src/client/components/menu/ItemDrop.tsx
+++ src/client/components/menu/ItemDrop.tsx
@@ -5,24 +5,19 @@ import { IconImg } from './Styled';
 //import { Character } from '../../App';
 
 interface ItemDropProps {
-<<<<<<< HEAD
- itemSlot: number;
- itemId: number;
- imageUrl: string;
- charLocation: number;
- charId: number;
-=======
   itemId: number;
   charLocation: number;
   imageUrl: string;
->>>>>>> 1da62c8f85c9e5b564a54aa8565b202f84e58b1d
+  charId: number;
+  itemSlot: number;
 }
 
 const ItemDrop: React.FC<ItemDropProps> = ({ itemId, charLocation, imageUrl, charId, itemSlot }) => {
+
   const handleClick = async () => {
     try {
       await axios.put(`/location/drop_item_slot/${charLocation}`, { drop_item_slot: itemId });
-      // await axios.patch(`/character/update/${charId}`, {`slot${itemSlot}`: null});
+      await axios.patch(`/character/update/${charId}`, {[`slot${itemSlot}`]: null});
     } catch (err) {
       console.error('Error updating location in handleClick--src/client/component/menu/ItemDrop.tsx: ', err);
     }
