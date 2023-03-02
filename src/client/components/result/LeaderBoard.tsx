4e56e3a09eb59b91036eadad5160b43faabc62bb
Sean Niehus <seanniehus@gmail.com>
2023-03-01 23:16:22 -0600

    leaderboard now rendering dynamic data

--- src/client/components/result/LeaderBoard.tsx
+++ src/client/components/result/LeaderBoard.tsx
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react';//
 
 import { Table, Th, TableRow, Td } from './Styled';
-import axios from 'axios';
 
 // import { UserContext } from '../../App'; // <-- holds User object
 
 
-// import axios from 'axios';
+import axios from 'axios';
+interface Leader {
+  name: string;
+  score: number;
+}
 
 const LeaderBoard: React.FC = () => {
 
@@ -24,38 +27,26 @@ const LeaderBoard: React.FC = () => {
     getLeaders();
   }, []);
 
+
+
   console.log('LEADERS', leaders);
   return (
-
     <Table>
       <thead>
         <tr>
           <Th>Character</Th>
-          <Th>User</Th>
-          <Th>HighScore</Th>
+          <Th>Score</Th>
         </tr>
       </thead>
       <tbody>
-
-        <TableRow>
-          <Td>Mr.Okra</Td>
-          <Td>20</Td>
-          <Td>29</Td>
-        </TableRow>
-        <TableRow>
-          <Td>Morgus</Td>
-          <Td>2</Td>
-          <Td>100</Td>
-        </TableRow>
-        <TableRow>
-          <Td>Borris Fart</Td>
-          <Td>15</Td>
-          <Td>55</Td>
-        </TableRow>
-
+        {leaders.map((leader: Leader, i: number) => (
+          <TableRow key={i}>
+            <Td>{leader.name}</Td>
+            <Td>{leader.score}</Td>
+          </TableRow>
+        ))}
       </tbody>
     </Table>
-
   );
 };
 
