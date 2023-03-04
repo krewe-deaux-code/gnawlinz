1c0693ee4b4fc5022729a7bae47cebc8b578249b
Miles Ager <miles.ager13@gmail.com>
2023-03-04 14:43:43 -0600

    fix screen refit to page

--- src/client/components/gameView/Styled.tsx
+++ src/client/components/gameView/Styled.tsx
@@ -153,8 +153,10 @@ export const StatIconContainer = styled.div`
 export const IconImg = styled.img`
   cursor: pointer;
   padding: auto;
-  width: 2em;
-  height: 2em;
+  width: auto;
+  height: auto;
+  max-width: 2em;
+  max-height: 2em;
   margin-left: auto;
   margin-right: auto;
 `;
