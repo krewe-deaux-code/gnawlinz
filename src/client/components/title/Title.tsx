cd9666da43f1704e5f0b8a89368574d6be860d3d
colin <colin.williams91@gmail.com>
2023-03-07 23:24:22 -0600

    extended titleLogoImg and TitleImg styled components into motion.img to add blur animation for Title

--- src/client/components/title/Title.tsx
+++ src/client/components/title/Title.tsx
@@ -1,7 +1,7 @@
 import React from 'react'; // { useEffect, useContext }
 //import { Link } from 'react-router-dom';
 import Nav from '../nav/NavBar';
-import { TitleContainer, Main, Footer, TitleLogoImg } from './Styled';
+import { TitleContainer, Main, Footer, TitleLogoImg, TitleImg } from './Styled';
 
 
 const Title: React.FC = () => {
@@ -10,8 +10,17 @@ const Title: React.FC = () => {
     <TitleContainer>
       <Nav isActive={false} />
       <Main>
-        <TitleLogoImg src='https://res.cloudinary.com/de0mhjdfg/image/upload/v1678237232/gnawlinzIcons/GnawlinzFinalColor_i3fncp.png' />
-        <img src='https://media.discordapp.net/attachments/1054143394194149408/1075618769109528656/TitleScreenV4.png' />
+        <TitleLogoImg
+          src='https://res.cloudinary.com/de0mhjdfg/image/upload/v1678237232/gnawlinzIcons/GnawlinzFinalColor_i3fncp.png'
+          initial={{ filter: 'blur(5px)' }}
+          animate={{ filter: 'blur(0px)' }}
+          transition={{ duration: .75 }}
+        />
+        <TitleImg src='https://media.discordapp.net/attachments/1054143394194149408/1075618769109528656/TitleScreenV4.png'
+          initial={{ filter: 'blur(6px)' }}
+          animate={{ filter: 'blur(0px)' }}
+          transition={{ duration: 1.25 }}
+        />
       </Main>
       <Footer>
         <button>
