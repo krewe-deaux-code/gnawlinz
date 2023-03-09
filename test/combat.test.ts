f7910c7cb0bcf43e3206ccf7b6afc923e80b39a0
Benjamin Dickinson <bsadickinson@gmail.com>
2023-03-09 11:59:04 -0600

    combat test file updated

--- test/combat.test.ts
+++ test/combat.test.ts
@@ -1,19 +1,39 @@
+import axios from 'axios';
 import { describe, expect, it } from 'vitest';
 import { Enemy } from '../src/client/App';
 import { statCheck, fightEnemy, isEnemy } from '../src/client/utility/gameUtils';
-
+import handleLocationChange from '../src/client/components/gameView/GameView';
 
 describe('#fightEnemy', () => {
-  it('should return an object', ()=> {
-    expect( typeof fightEnemy(5, 100, 7, 10)as string).toBe('object');
+  it('should return an object', () => {
+    expect(typeof fightEnemy(5, 100, 7, 10) as string).toBe('object');
   });
-  it('returned object should have a player or enemy property', ()=> {
-    expect(fightEnemy(5, 100, 7, 10)as object).toHaveProperty('enemy');
+  it('returned object should have a player or enemy property', () => {
+    expect(fightEnemy(5, 100, 7, 10) as object).toHaveProperty('enemy');
   });
-  it('returned object should have a player or enemy property', ()=> {
-    expect(fightEnemy(7, 100, 5, 10)as object).toHaveProperty('player');
+  it('returned object should have a player or enemy property', () => {
+    expect(fightEnemy(7, 100, 5, 10) as object).toHaveProperty('player');
   });
 });
+
+
+describe('#damageApplication', () => {
+  // const damage = fightEnemy(7, 100, 4, 10); // returns ~8
+  let playerHealth = 0;
+  axios.patch('character/update/1', { health: fightEnemy(7, 100, 4, 10).player })
+    .then(() => {
+      axios.get('character/1')
+        .then((response) => {
+          playerHealth += response.data.health;
+        });
+    });
+  // playerHealth should be 4-9
+  it('should provide a new health total to be saved in the DB', () => {
+    expect(playerHealth).toBeLessThan(10);
+  });
+});
+
+
 
 //export const fightEnemy = (enemyStrength: number, enemyHealth: number, playerStrength: number, playerHealth: number) => {
 
