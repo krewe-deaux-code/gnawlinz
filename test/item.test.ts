92cba2b09d892a7b2135b25ae6f16ea6d2e04f44
Miles Ager <miles.ager13@gmail.com>
2023-03-10 11:51:19 -0600

    remove pseudocode

--- test/item.test.ts
+++ test/item.test.ts
@@ -51,21 +51,3 @@ describe('#getItem Lead Pipe', () => {
       }));
   });
 });
-
-// describe('#damageApplication', () => {
-//   // const damage = fightEnemy(7, 100, 4, 10); // returns ~8
-//   let playerHealth = 0;
-//   axios.patch('/character/update/1', { health: fightEnemy(7, 100, 4, 10).player })
-//     .then(() => {
-//       axios.get('/character/1')
-//         .then((response) => {
-//           playerHealth += response.data.health;
-//         })
-//         .catch((err => console.log('err in damage calc tests line 29: ', err)));
-//     })
-//     .catch((err => console.log('err in damage calc tests line 31: ', err)));
-//   // playerHealth should be 4-9
-//   it('should provide a new health total to be saved in the DB', () => {
-//     expect(playerHealth).toBeLessThan(10);
-//   });
-// });
