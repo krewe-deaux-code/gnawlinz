d70d4f0f260e92964d5d5c2d10901c759775fc68
colin <colin.williams91@gmail.com>
2023-03-30 15:45:58 -0500

    begin nic uncaged boss, fixed red x position

--- src/db/seeders/seedData/bossSeed.ts
+++ src/db/seeders/seedData/bossSeed.ts
@@ -1,26 +1,26 @@
 export const bossSeed = [
   {
-    name: 'Nick Un-Caged',
-    image_url: 'https://helios-i.mashable.com/imagery/articles/03LBXfj2MXzQUTY5HiTEjih/images-10.fill.size_2000x1125.v1650320005.jpg',
-    weapon1: 'Pistols Akimbo',
+    name: 'Nicolas Un-Caged',
+    image_url: 'https://res.cloudinary.com/de0mhjdfg/image/upload/v1680208175/GnawlinzEnemies/Mack_1ed_zomb_pixyy7.webp',
+    weapon1: 'Kukri',
     strength: 5,
-    health: 20,
-    location: 3
+    health: 500,
+    location: Math.floor(Math.random() * 5 + 1)
   },
-  {
-    name: 'Bananasaurus',
-    image_url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/357.png',
-    weapon1: 'Banana Bunch',
-    strength: 3,
-    health: 20,
-    location: 1
-  },
-  {
-    name: 'Literally a Gundam',
-    image_url: 'https://media.graphassets.com/LmSp40GcTiWNTPsv2Gd9',
-    weapon1: 'Beam Rifle',
-    strength: 4,
-    health: 100,
-    location: 2
-  }
+  // {
+  //   name: 'Bananasaurus',
+  //   image_url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/357.png',
+  //   weapon1: 'Banana Bunch',
+  //   strength: 3,
+  //   health: 20,
+  //   location: 1
+  // },
+  // {
+  //   name: 'Literally a Gundam',
+  //   image_url: 'https://media.graphassets.com/LmSp40GcTiWNTPsv2Gd9',
+  //   weapon1: 'Beam Rifle',
+  //   strength: 4,
+  //   health: 100,
+  //   location: 2
+  // }
 ];
