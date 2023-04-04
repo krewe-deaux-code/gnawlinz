088e5ef7eee93f39ad8de433f736c7905358f2ef
Miles Ager <miles.ager13@gmail.com>
2023-04-04 16:55:24 -0500

    remove chages

--- src/client/components/menu/Styled.tsx
+++ src/client/components/menu/Styled.tsx
@@ -11,7 +11,7 @@ export const MenuContainer = styled.div`
 
 
 export const ArcadeBackground = styled.div`
-  /* width: 100%; */
+  width: 100%;
   height: calc(100vh - 767.33px);
   border-radius: 1rem 1rem 0 0;
   /* background: url('https://res.cloudinary.com/de0mhjdfg/image/upload/v1679955903/gnawlinzIcons/purple_cross_stripes_vtgu6o.png'); */
@@ -561,6 +561,8 @@ animation: ${CRTGlowPulse} .15s infinite;
 `;
 
 export const ArcadeGlowContainer = styled.div`
+  width: 100%;
+  height: 100%;
   justify-content: center;
   background: rgb(31, 33, 40);
   color: white;
